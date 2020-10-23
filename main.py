165795fe1d0da16456e1d30900a94290d8440466
Miftah Fauzy <miftah.fauzy@gmail.com>
2020-10-23 13:48:43 +0700

    Adding on event trap for connect and disconnect database

--- main.py
+++ main.py
@@ -30,6 +30,16 @@ metadata.create_all(engine)
 api = FastAPI()
 
 
+@api.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@api.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
 # create model for List all users
 class UserList(BaseModel):
     id: str
